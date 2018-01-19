ebf4ef2ab7b96d47e369af711e68c3667843bc34
Nikolaj ?ujskij <skrattaren@yandex.ru>
2018-01-20 00:00:02 +0300

    st: solarize!

--- st-0.7.h
+++ st-0.7.h
@@ -83,32 +83,36 @@ static char termname[] = "xterm-256color";
 static unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
+/* Dark Solarized theme by Ethan Schoonover (http://ethanschoonover.com/solarized)
+ * stolen from https://gist.github.com/gregf/5279175 */
+/* Requires "no bold" patch:
+ * https://st.suckless.org/patches/disable_bold_italic_fonts/ */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#073642",
+	"#dc322f",
+	"#859900",
+	"#b58900",
+	"#268bd2",
+	"#d33682",
+	"#2aa198",
+	"#eee8d5",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#002b36",
+	"#cb4b16",
+	"#586e75",
+	"#657b83",
+	"#839496",
+	"#6c71c4",
+	"#93a1a1",
+	"#fdf6e3",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
-	"#555555",
+	"#333333",
 };
 
 
